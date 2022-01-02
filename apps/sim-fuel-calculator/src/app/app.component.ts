4e7480e163b8e19b761439ac010b66221b6dae4e
Fibinger Ádám <adam.fibinger@wup.hu>
2022-01-02 10:45:32 +0100

    initial commit

---
+++ apps/sim-fuel-calculator/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sim-utils-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'sim-fuel-calculator';
+}
