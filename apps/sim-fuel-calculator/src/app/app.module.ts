02cbf4a5953a806b10aa486ec024bdc8eefc8c00
Fibinger Ádám <adam.fibinger@wup.hu>
2022-01-02 22:08:30 +0100

    First build v0.1

--- apps/sim-fuel-calculator/src/app/app.module.ts
+++ apps/sim-fuel-calculator/src/app/app.module.ts
@@ -3,10 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { UiModule } from '@sim-utils/ui';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { FuelCalculatorComponent } from './components/fuel-calculator/fuel-calculator.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NxWelcomeComponent, FuelCalculatorComponent],
+    imports: [
+        BrowserModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+            // Register the ServiceWorker as soon as the app is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000'
+        }),
+        UiModule,
+        StoreModule.forRoot(
+            {},
+            {
+                metaReducers: !environment.production ? [] : [],
+                runtimeChecks: {
+                    strictActionImmutability: true,
+                    strictStateImmutability: true
+                }
+            }
+        ),
+        EffectsModule.forRoot([]),
+        !environment.production ? StoreDevtoolsModule.instrument() : [],
+        BrowserAnimationsModule,
+        MatButtonToggleModule,
+        FormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSliderModule,
+        MatTooltipModule
+    ],
   providers: [],
   bootstrap: [AppComponent],
 })
